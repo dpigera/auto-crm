352252b11d075b26924de6953ec433b13625edc9
dpigera <dpigera@gmail.com>
2025-01-22 03:08:26 +0530

    Added back button to ticket-info

--- app/controllers/dashboard/tickets/view.js
+++ app/controllers/dashboard/tickets/view.js
@@ -1,8 +1,14 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class DashboardTicketsViewController extends Controller {
   @service router;
   @tracked ticket = this.model;
+
+  @action
+  goBack() {
+    this.router.transitionTo('dashboard.tickets');
+  }
 } 
