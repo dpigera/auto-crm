8251aa0015c1ce4b3a2c9a83db47fc90c4df75d7
dpigera <dpigera@gmail.com>
2025-01-24 19:41:50 +0530

    Add user detail view columns

--- app/controllers/dashboard/users/view.js
+++ app/controllers/dashboard/users/view.js
@@ -1,19 +1,26 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class DashboardUsersViewController extends Controller {
   @service pocketbase;
+  @service router;
   @tracked user;
-
 
   reloadData() {
     this.user = null;
     this.user = this.model;
   }
-  
+
   setup(model) {
     debugger;
     this.user = model;
+  }
+
+
+  @action
+  goBack() {
+    this.router.transitionTo('dashboard.users');
   }
 } 
