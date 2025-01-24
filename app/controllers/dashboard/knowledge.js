95c5a6c823208eb59eb7b5193eeb315529d7ccb9
dpigera <dpigera@gmail.com>
2025-01-25 00:51:50 +0530

    Added 'knowledge base' detail view

--- app/controllers/dashboard/knowledge.js
+++ app/controllers/dashboard/knowledge.js
@@ -1,6 +1,7 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class DashboardKnowledgeController extends Controller {
   @service pocketbase;
@@ -10,4 +11,5 @@ export default class DashboardKnowledgeController extends Controller {
     super(...arguments);
     // Future initialization code can go here
   }
+
 } 
